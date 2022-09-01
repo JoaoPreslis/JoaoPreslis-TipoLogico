9f1388e3a9ca378c5898ea73563209a3a3ec4c60
JoaoPreslis <110571542+JoaoPreslis@users.noreply.github.com>
2022-08-31 21:32:49 -0300

    Add files via upload

---
+++ TipoLogico.java
@@ -0,0 +1,19 @@
+package _9_logica_3_7;
+
+public class TipoLogico {
+    public static void main(String[] args) {
+        Boolean variavelVerdadeira = true;
+        System.out.println("Variavel verdadeira: " + variavelVerdadeira);
+        Boolean variavelFalsa = false;
+        System.out.println("Variável falsa: " + variavelFalsa);
+        System.out.println("------------------------------------------");
+        Integer idade = 20;
+        Boolean podeTirarCarteira = idade >= 18;
+        //System.out.println("Pode tirar carteira:" + podeTirarCarteira);
+        if (podeTirarCarteira){
+            System.out.println("Sim! Ele(a) pode tirar carteira.");
+        } else {
+            System.out.println("Não ! Ele(a) nao pode tirar carteira ");
+        }
+    }
+}
